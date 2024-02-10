4375720a27af41d02fad3d660bc40d10e81bd05c
Erik Antonio Alarcon Pinedo <ealarcop@emeal.nttdata.com>
2024-02-10 16:49:31 -0500

    x

--- src/main/java/gob/regionancash/obresec/controller/SSHController.java
+++ src/main/java/gob/regionancash/obresec/controller/SSHController.java
@@ -4,8 +4,10 @@ import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.List;
 
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import com.jcraft.jsch.*;
@@ -22,40 +24,58 @@ public class SSHController {
     @ConfigProperty(name = "ssh.password")
     String password;
 
-    @GET
+    @POST
     @Path("/execute")
+    @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.TEXT_PLAIN)
-    public String executeSSHCommand(@QueryParam("command") String command) {
-        JSch jsch = new JSch();
+    public String executeSSHCommands(List<String> commands) {
+        StringBuilder output = new StringBuilder();
         try {
-            Session session = jsch.getSession(user, host);
-            session.setPassword(password);
-            session.setConfig("StrictHostKeyChecking", "no");
+            Session session = createSession();
             session.connect();
+            for (String command : commands) {
+                output.append(executeSingleSSHCommand(session, command)).append("\n\n");
+            }
+            session.disconnect();
+        } catch (JSchException e) {
+            e.printStackTrace();
+            output.append("Error executing SSH commands: ").append(e.getMessage());
+        }
+        return output.toString();
+    }
 
+    private Session createSession() throws JSchException {
+        JSch jsch = new JSch();
+        Session session = jsch.getSession(user, host);
+        session.setPassword(password);
+        session.setConfig("StrictHostKeyChecking", "no");
+        return session;
+    }
+
+    private String executeSingleSSHCommand(Session session, String command) {
+        StringBuilder output = new StringBuilder();
+        try {
             ChannelExec channel = (ChannelExec) session.openChannel("exec");
             channel.setCommand(command);
-
-            channel.connect();
-            StringBuilder output = new StringBuilder();
-        InputStream errorStream = channel.getErrStream();
-        BufferedReader errorReader = new BufferedReader(new InputStreamReader(errorStream));
-        String errorLine;
-        while ((errorLine = errorReader.readLine()) != null) {
-            output.append("ERROR: ").append(errorLine).append("\n");
-        }
-
-            
+            channel.connect();    
+            InputStream errorStream = channel.getErrStream();
+            BufferedReader errorReader = new BufferedReader(new InputStreamReader(errorStream));
+            String errorLine;
+            while ((errorLine = errorReader.readLine()) != null) {
+                output.append("ERROR: ").append(errorLine).append("\n");
+            }
             byte[] tmp = new byte[1024];
-            output.append("OUTPUT:"+command+":");
+            output.append("OUTPUT:" + command + ":");
             while (true) {
                 while (channel.getInputStream().available() > 0) {
                     int i = channel.getInputStream().read(tmp, 0, 1024);
-                    if (i < 0) break;
+                    if (i < 0)
+                        break;
                     output.append(new String(tmp, 0, i));
                 }
                 if (channel.isClosed()) {
-                    if (channel.getInputStream().available() > 0) continue;
+                    if (channel.getInputStream().available() > 0)
+                        continue;
                     break;
                 }
                 try {
@@ -64,11 +84,10 @@ public class SSHController {
                 }
             }
             channel.disconnect();
-            session.disconnect();
-            return output.toString();
-        } catch (JSchException | java.io.IOException e) {
+        } catch (JSchException | IOException e) {
             e.printStackTrace();
-            return "Error executing SSH command: " + e.getMessage();
+            output.append("Error executing SSH command: ").append(e.getMessage());
         }
+        return output.toString();
     }
 }
