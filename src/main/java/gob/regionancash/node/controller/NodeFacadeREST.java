60b93b634eb418c22d12b34f7bf3eb4ad165b235
Erik Antonio Alarcon Pinedo <ealarcop@TRJ-M76GC7X5WV.local>
2024-03-30 17:44:22 -0500

    x

--- src/main/java/gob/regionancash/node/controller/NodeFacadeREST.java
+++ src/main/java/gob/regionancash/node/controller/NodeFacadeREST.java
@@ -4,24 +4,6 @@ import gob.regionancash.node.model.MenuRouter;
 import gob.regionancash.node.model.Node;
 import gob.regionancash.node.model.NodeRevision;
 import gob.regionancash.node.model.UrlAlias;
-import gob.regionancash.obresec.model.CrimeCategory;
-import gob.regionancash.obresec.service.CrimeCategoryFacadeLocal;
-import io.quarkus.panache.common.Parameters;
-
-import java.util.HashMap;
-
-import jakarta.inject.Inject;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.DELETE;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.PUT;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.QueryParam;
-import jakarta.ws.rs.core.MediaType;
-
 
 @Path("node")
 public class NodeFacadeREST {
