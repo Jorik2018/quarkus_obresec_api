7ff829c08bc773ab69d4edbc69fcd0e5ea82999a
Erik Antonio Alarcon Pinedo <ealarcop@TRJ-M76GC7X5WV.local>
2024-03-30 16:53:14 -0500

    x

--- src/main/java/gob/regionancash/node/controller/FileFacadeREST.java
+++ src/main/java/gob/regionancash/node/controller/FileFacadeREST.java
@@ -1,26 +1,19 @@
 package gob.regionancash.node.controller;
 
-import gob.regionancash.node.model.MenuRouter;
-import gob.regionancash.node.model.Node;
-import gob.regionancash.node.model.NodeRevision;
-import gob.regionancash.node.model.UrlAlias;
-import gob.regionancash.obresec.model.CrimeCategory;
-import gob.regionancash.obresec.service.CrimeCategoryFacadeLocal;
-import io.quarkus.panache.common.Parameters;
-
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Map;
 
-import jakarta.inject.Inject;
+import org.jboss.resteasy.reactive.PartType;
+
+import jakarta.annotation.security.PermitAll;
 import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.FormParam;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
-import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
@@ -30,10 +23,9 @@ import jakarta.ws.rs.core.Response;
 public class FileFacadeREST {
 
     @POST
-    public Object get(Map m) {
+    public Object get(Map<String, Object> m) {
         File directory = new File((String)m.get("folder"));
-        ArrayList list = new ArrayList<>();
-        // Get list of files and directories in the directory
+        ArrayList<Map<String, Object>> list = new ArrayList<>();
         File[] files = directory.listFiles();
         if (files != null) {
             for (File file : files) {
@@ -43,8 +35,17 @@ public class FileFacadeREST {
         return Map.of("data", list);
     }
 
+    @POST
+    @Path("download")
+    @PermitAll
+    @Produces(MediaType.APPLICATION_OCTET_STREAM)
+    public Response downloadFilePost(Map<String, Object> m) {
+        return this.downloadFile((String)m.get("folder"));
+    }
+
     @GET
-    @Path("/download")
+    @Path("download")
+    @PermitAll
     @Produces(MediaType.APPLICATION_OCTET_STREAM)
     public Response downloadFile(@QueryParam("filename") String filename) {
         try {
@@ -56,6 +57,28 @@ public class FileFacadeREST {
         } catch (FileNotFoundException e) {
             return Response.serverError().entity("Error downloading file: " + e.getMessage()).build();
         }
+    }
+    
+    @POST
+    @Path("upload")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    public Object upload(MultipartBody body) throws IOException {
+        byte[] fileBytes = body.file.readAllBytes();
+        String filePath = body.dst;
+        Files.write(Paths.get(filePath), fileBytes);
+        return Map.of("file",body.dst, "path", Paths.get(filePath).toFile().getAbsolutePath());
+    }
+
+    public static class MultipartBody {
+
+        @FormParam("file")
+        @PartType(MediaType.APPLICATION_OCTET_STREAM)
+        public InputStream file;
+
+        @FormParam("dst")
+        @PartType(MediaType.TEXT_PLAIN)
+        public String dst;
+
     }
     
 }
