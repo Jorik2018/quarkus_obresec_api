9875ff29ca16f2314f5fbe57da4d601f561de427
Erik Antonio Alarcón Pinedo <C8VMM@ferrovial.com>
2026-09-06 18:07:08 -0500

    x

--- src/main/java/gob/regionancash/node/controller/FileFacadeREST.java
+++ src/main/java/gob/regionancash/node/controller/FileFacadeREST.java
@@ -9,7 +9,8 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 import org.jboss.resteasy.reactive.PartType;
-
+import jakarta.ws.rs.NotFoundException;
+import jakarta.ws.rs.InternalServerErrorException;
 import jakarta.annotation.security.PermitAll;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.FormParam;
