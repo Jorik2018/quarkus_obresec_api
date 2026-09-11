c4ee15838d76385316ef254633d4524b00f15586
Erik Antonio Alarcón Pinedo <C8VMM@ferrovial.com>
2026-09-11 07:32:52 -0500

    x

--- src/main/java/gob/regionancash/content/controller/BlockFacadeREST.java
+++ src/main/java/gob/regionancash/content/controller/BlockFacadeREST.java
@@ -18,6 +18,7 @@ import jakarta.transaction.Transactional;
 import io.quarkus.panache.common.Parameters;
 import gob.regionancash.content.model.MenuRouter;
 import gob.regionancash.content.model.Node;
+import gob.regionancash.content.model.Box;
 import gob.regionancash.content.model.Block;
 import gob.regionancash.content.model.NodeRevision;
 import gob.regionancash.content.model.UrlAlias;
@@ -57,112 +58,32 @@ public class BlockFacadeREST {
 		return Response.ok(revision).build();
 	}
 
-	@GET
-	@Path("{id}")
-	public Response load(
-			@PathParam("id") Integer id,
-			@QueryParam("ref") String ref) {
-
-		/*
-		 * Buscar node
-		 */
-		Node node = Node.findById(id);
-
-		if (node == null) {
-			return Response.status(Response.Status.NOT_FOUND).build();
-		}
-
-		/*
-		 * Cargar revisión actual
-		 */
-		if (node.getVid() != null) {
-			NodeRevision revision = NodeRevision.findById(node.getVid());
-			node.setRevision(revision);
-		}
-
-		/*
-		 * Datos adicionales
-		 */
-		Map<String, Object> ext = new HashMap<>();
-
-		if (ref != null && !ref.isBlank()) {
-			ext.put("ref", ref);
-		}
-
-		/*
-		 * Alias
-		 */
-		List<UrlAlias> aliases = UrlAlias.list(
-				"src = ?1",
-				"node/" + node.getId());
-
-		ext.put("urlAliasList", aliases);
-
-		/*
-		 * URL principal del nodo
-		 */
-		String url = aliases.stream()
-				.map(UrlAlias::getDst)
-				.filter(dst -> dst != null && !dst.isBlank())
-				.findFirst()
-				.orElse("node/" + node.getId());
-
-		node.setUrl("/" + url);
-
-		/*
-		 * Uploads
-		 */
-		/*
-		 * List<Upload> uploads = Upload.list(
-		 * "uploadPK.vid = ?1 AND list > 0 ORDER BY weight",
-		 * node.getVid());
-		 * 
-		 * List<Upload> galleryList = new ArrayList<>();
-		 * List<Upload> uploadList = new ArrayList<>();
-		 * 
-		 * for (Upload upload : uploads) {
-		 * if (upload.getList() > 1) {
-		 * galleryList.add(upload);
-		 * } else {
-		 * uploadList.add(upload);
-		 * }
-		 * }
-		 */
-
-		/*
-		 * El código antiguo recorría desde atrás y luego hacía reverse().
-		 * Separándolo así conservamos directamente el ORDER BY weight.
-		 */
-		// ext.put("galleryList", galleryList);
-		// ext.put("uploadList", uploadList);
-
-		/*
-		 * Comentarios
-		 */
-		/*
-		 * if (node.getComment() > 0) {
-		 * 
-		 * List<Comment> comments = Comment.list(
-		 * "nid = ?1 ORDER BY timestamp",
-		 * node.getId());
-		 * 
-		 * ext.put("comments", comments);
-		 * 
-		 * ext.put(
-		 * "links",
-		 * new Object[][] {
-		 * { "Comentar", "/node/comment" }
-		 * });
-		 * }
-		 */
-
-		node.setExt(ext);
-
-		return Response.ok(new NodeDTO(node)).build();
-	}
-
 	@Inject
 	EntityManager entityManager;
+
+	@GET
+	@Path("{id}")
+	public Response load(@PathParam("id") Integer id) {
+
+		Block b = Block.findById(id);
+
+		if (b == null) {
+			return Response
+					.status(Response.Status.NOT_FOUND)
+					.build();
+		}
+
+		if ("Block".equals(b.getModule())
+				&& b.getDelta() != null) {
+
+			Box box = Box.findById(
+					Long.valueOf(b.getDelta()));
+
+			b.setExt(box);
+		}
+
+		return Response.ok(b).build();
+	}
 
 	@GET
 	@Path("/{from}/{to}")
@@ -172,9 +93,9 @@ public class BlockFacadeREST {
 			@QueryParam("theme") String theme,
 			@QueryParam("sortField") String sortField) {
 
-		sortField="region";
+		sortField = "region";
 
-		theme="garland";
+		theme = "garland";
 
 		Map<String, Object> result = new HashMap<>();
 
---
+++ src/main/java/gob/regionancash/content/model/Box.java
@@ -0,0 +1,41 @@
+package gob.regionancash.content.model;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+import lombok.*;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "dru_boxes")
+public class Block extends PanacheEntityBase {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "bid")
+    private Integer bid;
+
+    @Basic(optional = false)
+  @Column(name = "body")
+  private String body;
+  
+  @Basic(optional = false)
+  @Column(name = "info")
+  private String info;
+  
+  @Basic(optional = false)
+  @Column(name = "format")
+  private short format;
+
+}
