d5b55dfe6e113b0d0243ab0167082555e5e79de5
Erik Antonio Alarcón Pinedo <C8VMM@ferrovial.com>
2026-09-11 07:07:33 -0500

    x

--- src/main/java/gob/regionancash/content/model/Block.java
+++ src/main/java/gob/regionancash/content/model/Block.java
@@ -9,6 +9,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
+import lombok.*;
 
 @Entity
 @Data
